eb8afffdca906401dd07142478133bba9376603c
BecaFrancine24 <109755359+BecaFrancine24@users.noreply.github.com>
2024-03-22 11:11:55 -0300

    finalizado o campo script

---
+++ script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_mvc DEFAULT CHARACTER SET utf8;
+USE contatoJS;
+
+CREATE TABLE `conatos` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nome` varchar(100) DEFAULT NULL,
+    `idade` number(3) DEFAULT NULL,
+    `fone` number(25) DEFAULT NULL,
+    `fone2` number(25) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3
